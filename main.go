3ebd40262dbf2555b97ce44972899f25d72f9ee6
Albert <dharmata@Alberts-MacBook-Air.local>
2024-05-22 19:02:53 +0300

    finished lexer and REPL

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"PLI/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	currUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is REPL for test programming language!\n",
+		currUser.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+}
